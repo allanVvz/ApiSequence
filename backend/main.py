29ec83f14d6991099743f4d6f47cba6b96e35b07
allanVvz <allanulisses@hotmail.com>
2024-10-26 16:58:06 -0300

    firts intergration of back and front

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,29 @@ import pandas as pd
 import uvicorn
 from indb import *
 from dataset import DataSet
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 dataSet_instances = []
 data_processor = DataProcessor()
 
+# Configuração do CORS
+origins = [
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir origens específicas
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload-csv")
-def upload_csv(file: UploadFile = File(...)):
+async def upload_csv(file: UploadFile = File(...)):
     # Ler o arquivo CSV enviado
     print("Lendo o arquivo CSV enviado...")
     df = pd.read_csv(file.file)
@@ -27,6 +42,7 @@ def upload_csv(file: UploadFile = File(...)):
         for index, row in data_processor.df.iterrows():
             data_set = DataSet(pgn=row['pgn'], byte=row['byte_column'], sequence=row['data'].tolist())
             data_processor.dataSet_instances.append(data_set)
+            return {"message": "Arquivo recebido com sucesso"}
     else:
         return {"message": "Erro ao processar o arquivo: Nenhum dado válido encontrado."}
 
@@ -37,7 +53,7 @@ def upload_csv(file: UploadFile = File(...)):
 
 
 @app.get("/get-results")
-def get_results():
+async def get_results():
     if data_processor.dataSet_instances:
         results = [data_set.__dict__ for data_set in data_processor.dataSet_instances]
         print(results)
